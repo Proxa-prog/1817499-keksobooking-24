9cb13851384d881ef431cf2adfd6e1e8de6f9c0f
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2021-10-30 18:07:41 +0400

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -1,18 +1,22 @@
 const formWindow = document.querySelector('.ad-form');
 const mapFiltersWindow = document.querySelector('.map__filters');
-const formFieldset = formWindow.children;
+const formFieldset = formWindow.querySelectorAll('fieldset');
 const mapFiltersChildren = mapFiltersWindow.children;
+const howManyRooms = formWindow.querySelector('#room_number');
+const howManyGuests = formWindow.querySelector('#capacity').querySelectorAll('option');
+
+
 
 const formDeactivation = () => {
   formWindow.classList.add('ad-form--disabled');
   mapFiltersWindow.classList.add('map__filters--disabled');
 
   for(let i = 0; i < formFieldset.length; i++) {
-    formFieldset[i].disabled = 1;
+    formFieldset[i].disabled = true;
   }
 
   for(let i = 0; i < mapFiltersChildren.length; i++) {
-    mapFiltersChildren[i].disabled = 1;
+    mapFiltersChildren[i].disabled = true;
   }
 };
 
@@ -22,13 +26,35 @@ const formActivation = () => {
   mapFiltersWindow.classList.remove('map__filters--disabled');
 
   for(let i = 0; i < formFieldset.length; i++) {
-    formFieldset[i].disabled = 0;
+    formFieldset[i].disabled = false;
   }
 
   for(let i = 0; i < mapFiltersChildren.length; i++) {
-    mapFiltersChildren[i].disabled = 0;
+    mapFiltersChildren[i].disabled = false;
   }
 };
 
-export {formDeactivation, formActivation};
 
+const ratioOfGuests = (evt) => {
+  const currentValue = evt.target.value;
+  for(let i = 0; i < howManyGuests.length; i++) {
+    if(currentValue === howManyGuests[i].value || howManyGuests[i].value < currentValue) {
+      howManyGuests[i].disabled = false;
+    } else {
+      howManyGuests[i].disabled = true;
+    }
+  }
+
+  if (currentValue === '4') {
+    for(let j = 0; j < howManyGuests.length; j++) {
+      howManyGuests[j].disabled = true;
+      if (currentValue === howManyGuests[j].value) {
+        howManyGuests[j].disabled = false;
+      }
+    }
+  }
+}
+
+howManyRooms.addEventListener('change', ratioOfGuests);
+
+export {formDeactivation, formActivation};
