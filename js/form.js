c5632c3f39bcaab6646f70445eea835d3468bf84
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2021-10-31 11:34:13 +0400

    Исправил функции, убрал изменения разметки

--- js/form.js
+++ js/form.js
@@ -1,7 +1,8 @@
 const formWindow = document.querySelector('.ad-form');
 const mapFiltersWindow = document.querySelector('.map__filters');
 const formFieldset = formWindow.querySelectorAll('fieldset');
-const mapFiltersChildren = mapFiltersWindow.children;
+const mapFiltersSelect = mapFiltersWindow.querySelectorAll('select');
+const mapFiltersFieldset = mapFiltersWindow.querySelector('fieldset');
 const howManyRooms = formWindow.querySelector('#room_number');
 const howManyGuests = formWindow.querySelector('#capacity').querySelectorAll('option');
 
@@ -13,9 +14,11 @@ const formDeactivation = () => {
     formFieldset[i].disabled = true;
   }
 
-  for(let i = 0; i < mapFiltersChildren.length; i++) {
-    mapFiltersChildren[i].disabled = true;
+  for(let i = 0; i < mapFiltersSelect.length; i++) {
+    mapFiltersSelect[i].disabled = true;
   }
+
+  mapFiltersFieldset.disabled = true;
 };
 
 
@@ -27,26 +30,28 @@ const formActivation = () => {
     formFieldset[i].disabled = false;
   }
 
-  for(let i = 0; i < mapFiltersChildren.length; i++) {
-    mapFiltersChildren[i].disabled = false;
+  for(let i = 0; i < mapFiltersSelect.length; i++) {
+    mapFiltersSelect[i].disabled = false;
   }
+
+  mapFiltersFieldset.disabled = false;
 };
 
 
 const ratioOfGuests = (evt) => {
   const currentValue = evt.target.value;
   for(let i = 0; i < howManyGuests.length; i++) {
-    if(currentValue === howManyGuests[i].value || howManyGuests[i].value < currentValue) {
+    if(currentValue === howManyGuests[i].value || howManyGuests[i].value < currentValue && howManyGuests[i].value !== '0') {
       howManyGuests[i].disabled = false;
     } else {
       howManyGuests[i].disabled = true;
     }
   }
 
-  if (currentValue === '4') {
+  if (currentValue === '100') {
     for(let j = 0; j < howManyGuests.length; j++) {
       howManyGuests[j].disabled = true;
-      if (currentValue === howManyGuests[j].value) {
+      if (howManyGuests[j].value === '0') {
         howManyGuests[j].disabled = false;
       }
     }
