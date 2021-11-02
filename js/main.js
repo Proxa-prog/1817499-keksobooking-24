8ce3bc35179c029c44bc1684455f590c011edd5e
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2021-11-02 19:48:27 +0400

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-import {createNewAnnouncementElement} from './similrArannouncement.js';
-import {ARRAY_LENGTH} from './arrays-and-variables.js';
-import {createAd} from './object-creation-functions.js';
-import {formDeactivation, formActivation} from './form.js';
+import './map.js';
+import {formDeactivation} from './form.js';
 
 formDeactivation();
-formActivation();
-const createArrayAd = Array.from({length: ARRAY_LENGTH}, createAd);
-createNewAnnouncementElement(createArrayAd[0]);
-
-
